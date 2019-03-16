3f29436226f4e3658d1a278548600da9fa0dbdea
James Wright <james.wright@yld.io>
2019-03-16 22:41:51 +0000

    Introduces text-based X rendering to inlined script

--- src/index.inlined.js
+++ src/index.inlined.js
@@ -1,5 +1,5 @@
 const audioContext = new AudioContext();
-const baseScale = [0, 3, 5, 7];
+const baseScale = [0, 3, 5, 7, 11];
 
 const lead = audioContext.createOscillator();
 const bass = audioContext.createOscillator();
@@ -9,25 +9,36 @@ gain.gain.value = 0.1;
 lead.type = 'square';
 bass.type = 'triangle';
 
-[bass, lead].map(n => {
-  n.connect(gain);
-  n.start();
-});
-
+bass.connect(gain);
+lead.connect(gain);
 gain.connect(audioContext.destination);
+lead.start();
+bass.start();
 
 // createGame();
 let level = 1;
 let score = 0;
+let iterationCount = 0;
 let over = false;
 
-const computeXPos = time =>
-  time % 2 | 0
-    ? [time % 2 | 0 ? -0.06 : 1, time % 1]
-    : [time % 1, time % 2 | 0 ? -0.06 : 1];
+/* Going to comment this function because
+ * it's more involved than the others */
+const computeXProps = (i, spawnOffsetMs = 0) => {
+  // the outer side at which the x sits, above or below the game world on the x or y axis
+  const outerScalar = Math.random() + 0.5 | 0 === 0 ? -0.06 : 1;
 
-const computeXSpeed = pos =>
-  pos.map(p =>
+  // the second coordinate _within_ the game world's bounds
+  const innerScalar = Math.random();
+
+  /* A means of randomising whether the outer or inner is
+   * used for the x or y value, which can be spread */
+  const pos = Math.random() + 0.5 | 0 === 0
+    ? [outerScalar, innerScalar]
+    : [innerScalar, outerScalar];
+
+  /* negate the pos vector so the x travels in the
+   * right direction. Sorry for the nested ternary! */
+  const speed = pos.map(p =>
     p === -0.06
       ? 0.004
       : p === 1
@@ -35,25 +46,46 @@ const computeXSpeed = pos =>
         : 0
   );
 
+  const spawnDelayMs = i * 1000 + spawnOffsetMs;
+
+  return [pos, speed, spawnDelayMs];
+};
+
+const resetX = (x, time) => {
+  const [pos, speed, spawnDelayMs] = computeXProps(0, time);
+
+  x.pos = pos;
+  x.speed = speed;
+  x.spawnDelayMs = spawnDelayMs;
+};
+
 // createPlayer
-let playerPos = [0.46, 0.46];
+let playerPos = [0.5 - 0.085 / 2, 0.5 - 0.085 / 2];
 let playerSpeed = [0.005, 0];
 let health = 1;
 
 const xs = [0, 1, 2]
-  .map(i => computeXPos(60 * i))
-  .map((pos, i) => ({
-    pos,
-    s: computeXSpeed(pos), // speed - reserved Terser prop, but overriding in config isn't working
-    deactivated: true,
-    spawnDelayMs: i * 1000,
-  }));
+  .map(i => {
+    const [pos, speed, spawnDelayMs] = computeXProps(i);
+
+    return {
+      pos,
+      size: 0.06,
+      speed,
+      spawnable: true,
+      deactivated: true,
+      spawnDelayMs,
+    };
+  });
 
 const keyboard = {};
 
 // this === window in this scope
 this.onkeydown = e => {
-  audioContext.resume(); // Chrome autoplay policy
+  if (audioContext.state === 'suspended') {
+    audioContext.resume(); // Chrome autoplay policy
+  }
+
   keyboard[e.key] = !over;
 };
 
@@ -61,17 +93,82 @@ this.onkeyup = e => {
   keyboard[e.key] = false;
 };
 
-const loop = now => {
-  const time = Math.max(now, 0);
-
+const loop = time => {
   // schedule music note change
-  if (time % 333 < 16.66) {
-    lead.frequency.value = 18.35 * 1.059 ** baseScale[time % 3 | 0] * 8;
-    bass.frequency.value = 18.35 * 1.059 ** baseScale[(time + 1) % 4 | 0] * 4;
-  }
+  lead.frequency.setValueAtTime(
+    18.35 * 1.0594 ** baseScale[Math.floor(Math.random() * (baseScale.length - 1))] * 8,
+    audioContext.currentTime + 0.3 * iterationCount,
+  );
 
+  bass.frequency.setValueAtTime(
+    18.35 * 1.0594 ** baseScale[Math.floor(Math.random() * (baseScale.length - 1))] * 4,
+    audioContext.currentTime + 0.3 * iterationCount,
+  );
+
+  c.clearRect(0, 0, a.width, a.height);
   c.fillStyle = '#000';
-  c.fillRect(0, 0, 98, 98);
+  c.fillRect(0, 0, a.width, a.height);
+
+  xs.forEach(x => {
+    if (x.deactivated && x.spawnDelayMs < time) {
+      x.deactivated = false;
+    }
+
+    if (x.deactivated) {
+      return;
+    }
+
+    if (x.pos.some(p => p < 0 - x.size || p > 1 + x.size)) {
+      resetX(x, time);
+    }
+
+    x.speed.forEach((speed, i) => {
+      x.pos[i] += speed * level / 3;
+    });
+
+    c.fillStyle = '#008';
+
+    c.translate((x.pos[0] + 0.06 / 2) * 480, (x.pos[1] + 0.06 / 2) * 480);
+    c.rotate(0.002 * time);
+    c.fillRect(-0.06 / 2 * 480, -0.06 / 2 * 480, 0.06 * 480, 0.06 * 480);
+
+    c.fillStyle = '#fff';
+    c.font = `${0.06 * 480}px sans-serif`;
+
+    c.fillText('X', (-0.06 * 480) / 2.9, (0.06 * 480) / 2.8);
+
+    c.resetTransform();
+
+    if (
+      playerPos[0] + 0.085 >= x.pos[0] &&
+      playerPos[0] <= x.pos[0] + x.size &&
+      playerPos[1] + 0.085 >= x.pos[1] &&
+      playerPos[1] <= x.pos[1] + x.size
+    ) {
+      health = 1;
+      score += 1;
+
+      if (score % 8 === 0) {
+        level++;
+      }
+
+      resetX(x, time);
+
+      const osc = audioContext.createOscillator();
+      const gain = audioContext.createGain();
+
+      osc.type = 'square';
+      osc.frequency.value = 166;
+
+      gain.gain.setValueAtTime(0.3, audioContext.currentTime);
+      gain.gain.exponentialRampToValueAtTime(0.00001, audioContext.currentTime + 0.3);
+
+      osc.connect(gain);
+      gain.connect(audioContext.destination);
+      osc.start();
+      osc.stop(audioContext.currentTime + 0.3);
+    }
+  });
 
   if (health <= 0) {
     over = true;
@@ -79,7 +176,7 @@ const loop = now => {
     health -= 0.002;
   }
 
-  playerSpeed.map((speed, i) => {
+  playerSpeed.forEach((speed, i) => {
     playerPos[i] += speed;
   });
 
@@ -89,98 +186,39 @@ const loop = now => {
   }
 
   c.fillStyle = '#fff';
-  c.translate((playerPos[0] + 0.0425) * 98, (playerPos[1] + 0.0425) * 98);
+  c.translate((playerPos[0] + 0.085 / 2) * 480, (playerPos[1] + 0.085 / 2) * 480);
   c.rotate(Math.atan2(playerSpeed[1], playerSpeed[0]));
 
   c.beginPath();
-  c.moveTo(-5.1, -5.1);
-  c.lineTo(5.1, 1.275);
-  c.lineTo(-5.1, 5.1);
+  c.moveTo(-0.085 / 2 * 480, -0.085 / 2 * 480);
+  c.lineTo(0.085 / 2 * 480, 0.085 / 8 * 480);
+  c.lineTo(-0.085 / 2 * 480, 0.085 / 2 * 480);
+  c.lineTo(-0.085 / 2 * 480, -0.085 / 2 * 480);
   c.closePath();
   c.fill();
   c.resetTransform();
 
-  xs.map(x => { // 'map'.length < 'forEach'.length
-    if (x.deactivated && x.spawnDelayMs < time) {
-      x.deactivated = false;
-    }
+  c.fillStyle = '#ff0';
 
-    if (!x.deactivated) {
-      if (x.pos.some(p => p < -0.06 || p > 1.06)) {
-        x.pos = computeXPos(time);
-        x.s = computeXSpeed(x.pos);
-        x.spawnDelayMs = time;
-      }
-
-      x.s.map((speed, i) => {
-        x.pos[i] += speed * level / 3 ;
-      });
-
-      c.fillStyle = '#ff0';
-
-      c.translate((x.pos[0] + 0.03) * 98, (x.pos[1] + 0.03) * 98);
-      c.rotate(0.002 * time);
-      c.fillRect(-3.6, -3.6, 7.2, 7.2);
-
-      c.beginPath();
-      c.moveTo(-2.4, -2.4);
-      c.lineTo(2.4, 2.4);
-      c.stroke();
-
-      c.beginPath();
-      c.moveTo(-2.4, 2.4);
-      c.lineTo(2.4, -2.4);
-      c.stroke();
-
-      c.resetTransform();
-
-      if (
-        playerPos[0] + 0.08 >= x.pos[0] &&
-        playerPos[0] <= x.pos[0] + 0.06 &&
-        playerPos[1] + 0.08 >= x.pos[1] &&
-        playerPos[1] <= x.pos[1] + 0.06
-      ) {
-        health = 1;
-        score += 1;
-
-        if (score % 8 === 0) {
-          level++;
-        }
-
-        x.pos = computeXPos(time);
-        x.s = computeXSpeed(x.pos);
-        x.spawnDelayMs = time;
-
-        const osc = audioContext.createOscillator();
-        const gain = audioContext.createGain();
-
-        osc.type = 'square';
-        osc.frequency.value = 166;
-        gain.gain.value = 0.2;
-
-        osc.connect(gain);
-        gain.connect(audioContext.destination);
-        osc.start(time / 1000);
-        osc.stop(time / 1000 + 0.05);
-      }
-    }
-  });
-
-  c.fillRect(6, 6, 0.9 * health * 98, 1.2);
+  c.fillRect(
+    0.05 * 480,
+    0.05 * 480,
+    (1 - 0.05 * 2) * health * 480,
+    (0.06 - 0.05) * 480,
+  );
 
   if (over) {
     c.fillStyle = '#fff';
-    c.fillText('😢', 54, 64);
+
+    c.fillText(
+      'Game Over!',
+      a.width / 2 - c.measureText('Game Over!').width / 2,
+      a.height / 2 - 8,
+    );
   }
 
+  iterationCount++;
   requestAnimationFrame(loop);
 };
-
-// sweet tricks to pixelate output
-a.width = a.height = 98;
-
-a.style.imageRendering = 'mozPaintCount' in this // shorter than user agent test
-  ? 'optimizeSpeed'
-  : 'pixelated';
 
 requestAnimationFrame(loop);
