8fb4c81b4a3db823c13a43c31b04226eb0a07cc0
James Wright <james.wright@yld.io>
2019-03-16 19:57:18 +0000

    Removes more redundant code

--- src/index.inlined.js
+++ src/index.inlined.js
@@ -35,14 +35,8 @@ const computeXSpeed = pos =>
         : 0
   );
 
-const resetX = (x, time) => {
-  x.pos = computeXPos(time);
-  x.s = computeXSpeed(x.pos);
-  x.spawnDelayMs = time;
-};
-
 // createPlayer
-let playerPos = [0.457, 0.457];
+let playerPos = [0.46, 0.46];
 let playerSpeed = [0.005, 0];
 let health = 1;
 
@@ -73,78 +67,12 @@ const loop = now => {
 
   // schedule music note change
   if (time % 333 < 16.66) {
-    lead.frequency.value = 18.35 * 1.0594 ** baseScale[time % 3 | 0] * 8;
-    bass.frequency.value = 18.35 * 1.0594 ** baseScale[(time + 1) % 4 | 0] * 4;
+    lead.frequency.value = 18.35 * 1.059 ** baseScale[time % 3 | 0] * 8;
+    bass.frequency.value = 18.35 * 1.059 ** baseScale[(time + 1) % 4 | 0] * 4;
   }
 
   c.fillStyle = '#000';
-  c.fillRect(0, 0, 120, 120);
-
-  xs.map(x => { // 'map'.length < 'forEach'.length
-    if (x.deactivated && x.spawnDelayMs < time) {
-      x.deactivated = false;
-    }
-
-    if (!x.deactivated) {
-      if (x.pos.some(p => p < -0.06 || p > 1.06)) {
-        resetX(x, time);
-      }
-
-      x.s.map((speed, i) => {
-        x.pos[i] += speed * level / 3 ;
-      });
-
-      c.fillStyle = '#008';
-
-      c.translate((x.pos[0] + 0.03) * 120, (x.pos[1] + 0.03) * 120);
-      c.rotate(0.002 * time);
-      c.fillRect(-3.6, -3.6, 7.2, 7.2);
-
-      c.strokeStyle = '#fff';
-
-      c.beginPath();
-      c.moveTo(-2.4, -2.4);
-      c.lineTo(2.4, 2.4);
-      c.closePath();
-      c.stroke();
-
-      c.beginPath();
-      c.moveTo(-2.4, 2.4);
-      c.lineTo(2.4, -2.4);
-      c.closePath();
-      c.stroke();
-
-      c.resetTransform();
-
-      if (
-        playerPos[0] + 0.085 >= x.pos[0] &&
-        playerPos[0] <= x.pos[0] + 0.06 &&
-        playerPos[1] + 0.085 >= x.pos[1] &&
-        playerPos[1] <= x.pos[1] + 0.06
-      ) {
-        health = 1;
-        score += 1;
-
-        if (score % 8 === 0) {
-          level++;
-        }
-
-        resetX(x, time);
-
-        const osc = audioContext.createOscillator();
-        const gain = audioContext.createGain();
-
-        osc.type = 'square';
-        osc.frequency.value = 166;
-        gain.gain.value = 0.2;
-
-        osc.connect(gain);
-        gain.connect(audioContext.destination);
-        osc.start(time / 1000);
-        osc.stop(time / 1000 + 0.05);
-      }
-    }
-  });
+  c.fillRect(0, 0, 98, 98);
 
   if (health <= 0) {
     over = true;
@@ -162,21 +90,84 @@ const loop = now => {
   }
 
   c.fillStyle = '#fff';
-  c.translate((playerPos[0] + 0.0425) * 120, (playerPos[1] + 0.0425) * 120);
+  c.translate((playerPos[0] + 0.0425) * 98, (playerPos[1] + 0.0425) * 98);
   c.rotate(Math.atan2(playerSpeed[1], playerSpeed[0]));
 
   c.beginPath();
   c.moveTo(-5.1, -5.1);
   c.lineTo(5.1, 1.275);
   c.lineTo(-5.1, 5.1);
-  c.lineTo(-5.1, -5.1);
   c.closePath();
   c.fill();
   c.resetTransform();
 
-  c.fillStyle = '#ff0';
+  xs.map(x => { // 'map'.length < 'forEach'.length
+    if (x.deactivated && x.spawnDelayMs < time) {
+      x.deactivated = false;
+    }
 
-  c.fillRect(6, 6, 0.9 * health * 120, 1.2);
+    if (!x.deactivated) {
+      if (x.pos.some(p => p < -0.06 || p > 1.06)) {
+        x.pos = computeXPos(time);
+        x.s = computeXSpeed(x.pos);
+        x.spawnDelayMs = time;
+      }
+
+      x.s.map((speed, i) => {
+        x.pos[i] += speed * level / 3 ;
+      });
+
+      c.fillStyle = '#ff0';
+
+      c.translate((x.pos[0] + 0.03) * 98, (x.pos[1] + 0.03) * 98);
+      c.rotate(0.002 * time);
+      c.fillRect(-3.6, -3.6, 7.2, 7.2);
+
+      c.beginPath();
+      c.moveTo(-2.4, -2.4);
+      c.lineTo(2.4, 2.4);
+      c.stroke();
+
+      c.beginPath();
+      c.moveTo(-2.4, 2.4);
+      c.lineTo(2.4, -2.4);
+      c.stroke();
+
+      c.resetTransform();
+
+      if (
+        playerPos[0] + 0.08 >= x.pos[0] &&
+        playerPos[0] <= x.pos[0] + 0.06 &&
+        playerPos[1] + 0.08 >= x.pos[1] &&
+        playerPos[1] <= x.pos[1] + 0.06
+      ) {
+        health = 1;
+        score += 1;
+
+        if (score % 8 === 0) {
+          level++;
+        }
+
+        x.pos = computeXPos(time);
+        x.s = computeXSpeed(x.pos);
+        x.spawnDelayMs = time;
+
+        const osc = audioContext.createOscillator();
+        const gain = audioContext.createGain();
+
+        osc.type = 'square';
+        osc.frequency.value = 166;
+        gain.gain.value = 0.2;
+
+        osc.connect(gain);
+        gain.connect(audioContext.destination);
+        osc.start(time / 1000);
+        osc.stop(time / 1000 + 0.05);
+      }
+    }
+  });
+
+  c.fillRect(6, 6, 0.9 * health * 98, 1.2);
 
   if (over) {
     c.fillStyle = '#fff';
@@ -187,7 +178,7 @@ const loop = now => {
 };
 
 // sweet tricks to pixelate output
-a.width = a.height = 120;
+a.width = a.height = 98;
 
 a.style.imageRendering = 'mozPaintCount' in this // shorter than user agent test
   ? 'optimizeSpeed'
